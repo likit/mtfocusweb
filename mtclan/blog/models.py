c76a7dadd0dfe3cdc0cd3eb855a6db405545c3ac
Likit Preeyanon <likit.pre@mahidol.edu>
2019-04-24 17:08:56 +0700

    added blog page model, page, template
    updated the layout of the blog index page

--- mtclan/blog/models.py
+++ mtclan/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from wagtail.core.models import Page
 from wagtail.core.fields import RichTextField
 from wagtail.admin.edit_handlers import FieldPanel
+from wagtail.search import index
 
 # Create your models here.
 
@@ -10,4 +11,21 @@ class BlogIndexPage(Page):
 
     content_panels = Page.content_panels + [
         FieldPanel('intro', classname='full')
+    ]
+
+
+class BlogPage(Page):
+    date = models.DateField("Post Date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body')
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname='full'),
     ]
